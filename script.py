8837edb119aba64c5d29a9fb92a08d981c2019d7
Stas <stasnovalenco3@gmail.com>
2025-03-15 12:46:52 +0200

    changed back to master

--- script.py
+++ script.py
@@ -2,14 +2,19 @@ from pynput.mouse import Listener, Button
 import time
 import threading
 
+
+def add_to_file(file_path, message):
+    with open(file_path, "a") as file:
+        file.write(message)
+
+
 interval= 0.5
 last_time= time.time()
-
-
 def on_move(x, y):
     global last_time
     current_time= time.time()
     if current_time - last_time >= interval:
+        add_to_file("moves.txt", f"{x, y}\n")
         print(x, y)
         last_time= current_time
 
@@ -18,26 +23,20 @@ def on_click(x, y, button, pressed):
     if pressed:
         if button == Button.left:
             print(f"Left click: {x, y}")
+            add_to_file("moves.txt", f"Left click: {x, y}\n")
         elif button == Button.right:
             print(f"Right click: {x, y}")
+            add_to_file("moves.txt", f"Right click: {x, y}\n")
         elif button == Button.middle:
             return False
 
-def mouse_listener():
-    with Listener(
-        on_move= on_move,
-        on_click= on_click
-    ) as listener:
-        listener.join()
 
-        return listener
+with Listener(
+    on_move= on_move,
+    on_click= on_click
+) as listener:
+    listener.join()
 
 
-def add_to_file(file_path, message):
-    with open(file_path, "a") as file:
-        file.write(message)
 
-
-# thread= threading.Thread(target= add_to_file, args=("follow moves/moves.txt", mouse_listener()))
-# thread.start()
 
